356f8870001b56f2f9abba179fa6c0a27761df23
KwaziCoder <63724691+KwaziCoder@users.noreply.github.com>
2021-08-22 23:55:20 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Header = () => {
+  return(
+    <header className="header">
+      <img src="https://w7.pngwing.com/pngs/452/28/png-transparent-warhammer-40-000-design-logo-monochrome-warhammer-40000.png" />
+    </header>
+
+  )
+}
+
+
+export default Header;
